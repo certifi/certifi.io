cf90002b73ed111efdc12aa187b0f5ac8a8f5ad6
Kenneth Reitz <me@kennethreitz.com>
2014-04-17 17:37:27 -0400

    don't show source :)

--- source/conf.py
+++ source/conf.py
@@ -45,6 +45,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'certifi'
 copyright = u"Copyright 2014, A <a href='http://kennethreitz.org/'>Kenneth Reitz</a> Project."
+show_source = False
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
